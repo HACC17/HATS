b3993e4e9cafa1e0db503d21b90703615cd35cac
KuakiniRob <kuakinir@gmail.com>
2017-09-11 19:55:59 -0700

    Clean Laravel

---
+++ laravel/routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+   
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/dockets', function () {
+    return view('dockets');
+});
+
+Route::get('/lucMaps', function () {
+    return view('lucMaps');
+});
+
+Route::get('/self-service', function () {
+    return view('self-service');
+});
