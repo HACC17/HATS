d3771482f66a2e7fcd8aa4e8ace9f86a7d36055b
KuakiniRob <kuakinir@gmail.com>
2017-09-14 06:05:45 -0700

    Phils lucmaps code into my dev-Rob page

--- laravel/resources/views/pages/contact.blade.php
+++ laravel/resources/views/pages/contact.blade.php
@@ -28,6 +28,7 @@ border: 5px solid red;
         }
 
 .locationBox{
+margin-right: 100px;
 border: 5px solid red;
 display: inline-block;
 vertical-align: top;
@@ -47,7 +48,7 @@ Honolulu, Hawai`i 96813<br>
 </p>
 </div>
 <div class="locationBox">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3717.0409905089614!2d-157.86026068506263!3d21.309396585847058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c006e759d0c0d79%3A0xabf9211f0f0d24e!2s235+S+Beretania+St%2C+Honolulu%2C+HI+96813!5e0!3m2!1sen!2sus!4v1505392296274" width="400" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3717.0409905089614!2d-157.86026068506263!3d21.309396585847058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c006e759d0c0d79%3A0xabf9211f0f0d24e!2s235+S+Beretania+St%2C+Honolulu%2C+HI+96813!5e0!3m2!1sen!2sus!4v1505392296274" width="500" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 <h2>Contact Information</h2>
 <p>You can reach us at:<br>
