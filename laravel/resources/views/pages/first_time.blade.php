8cfedb3a68cf64831c79382415fa2927accb82f6
KuakiniRob <kuakinir@gmail.com>
2017-09-16 23:10:15 -0700

    first time css formatting

--- laravel/resources/views/pages/first_time.blade.php
+++ laravel/resources/views/pages/first_time.blade.php
@@ -23,11 +23,9 @@
         margin-bottom: 1in;
         padding: 20px;
         background-color: rgba(248,248,248,0.8);
-        border: 6px solid red;
+
         }
         .pagecontainer {
-        border: 6px solid red;
-        display: inline-block
         }
         .center {
         text-align: center;
@@ -84,13 +82,29 @@
         box-shadow: 1px 1px 3px #000;
         }
         .close:hover { background: #00d9ff; }
+
+.modalCon{
+vertical-align: top;
+height: 100px;
+width: 200px;
+margin-left: 50px;
+margin-bottom: 50px;
+display: inline-block;
+background-color: rgba(248,248,248);
+}
+
+.modalBox{
+border: 1px solid red;
+}
     </style>
     <body>
         <div class="pagecontainer">
             <div class="textcontainer">
                 <h2> If you see a question below you want to know the answer to, click it for information.</h2>
             </div>
-            <a href="#openModal1">What does LUC mean?</a>
+<div class="modalBox">
+<!-- Modal:01 -->
+            <a class="modalCon" href="#openModal1"><p>What does LUC mean?</p></a>
             <div id="openModal1" class="modalDialog">
                 <div>
                     <a href="#close" title="Close" class="close">X</a>
@@ -98,7 +112,8 @@
                     <p>LUC is the abbreviation for Land Use Commission</p>
                 </div>
             </div>
-            <a href="#openModal2">What services does LUC provide?</a>
+<!-- Modal:02 -->
+            <a class="modalCon" href="#openModal2">What services does LUC provide?</a>
             <div id="openModal2" class="modalDialog">
                 <div>
                     <a href="#close" title="Close" class="close">X</a>
@@ -106,6 +121,85 @@
                     <p>LUC provides assistance with land property information, processing this request, and compiling resources for the public.</p>
                 </div>
             </div>
+<!-- Modal:03 -->
+            <a class="modalCon" href="#openModal3">Where can I learn more about LUC?</a>
+            <div id="openModal3" class="modalDialog">
+                <div>
+                    <a href="#close" title="Close" class="close">X</a>
+                    <h2>What does LUC mean?</h2>
+                    <p>LUC is the abbreviation for Land Use Commission</p>
+                </div>
+            </div>
+<!-- Modal:04 -->
+            <a class="modalCon" href="#openModal4">What are dockets?</a>
+            <div id="openModal4" class="modalDialog">
+                <div>
+                    <a href="#close" title="Close" class="close">X</a>
+                    <h2>What does LUC mean?</h2>
+                    <p>LUC is the abbreviation for Land Use Commission</p>
+                </div>
+            </div>
+<!-- Modal:05 -->
+            <a class="modalCon" href="#openModal5">What do I need to process a request?</a>
+            <div id="openModal5" class="modalDialog">
+                <div>
+                    <a href="#close" title="Close" class="close">X</a>
+                    <h2>What does LUC mean?</h2>
+                    <p>LUC is the abbreviation for Land Use Commission</p>
+                </div>
+            </div>
+
+<!-- Modal:06 -->
+            <a class="modalCon" href="#openModal6">Where can I get additional assistance?</a>
+            <div id="openModal6" class="modalDialog">
+                <div>
+                    <a href="#close" title="Close" class="close">X</a>
+                    <h2>What does LUC mean?</h2>
+                    <p>LUC is the abbreviation for Land Use Commission</p>
+                </div>
+            </div>
+
+<!-- Modal:07 -->
+            <a class="modalCon" href="#openModal7">What is a Tax map key?</a>
+            <div id="openModal7" class="modalDialog">
+                <div>
+                    <a href="#close" title="Close" class="close">X</a>
+                    <h2>What does LUC mean?</h2>
+                    <p>LUC is the abbreviation for Land Use Commission</p>
+                </div>
+            </div>
+
+<!-- Modal:08 -->
+            <a class="modalCon" href="#openModal8">Where is LUC located?</a>
+            <div id="openModal8" class="modalDialog">
+                <div>
+                    <a href="#close" title="Close" class="close">X</a>
+                    <h2>What does LUC mean?</h2>
+                    <p>LUC is the abbreviation for Land Use Commission</p>
+                </div>
+            </div>
+
+<!-- Modal:09 -->
+            <a class="modalCon" href="#openModal9">How do I obtain a Tax map key?</a>
+            <div id="openModal9" class="modalDialog">
+                <div>
+                    <a href="#close" title="Close" class="close">X</a>
+                    <h2>What does LUC mean?</h2>
+                    <p>LUC is the abbreviation for Land Use Commission</p>
+                </div>
+            </div>
+
+<!-- Modal:10 -->
+            <a class="modalCon" href="#openModal10">What are LUC maps?</a>
+            <div id="openModal10" class="modalDialog">
+                <div>
+                    <a href="#close" title="Close" class="close">X</a>
+                    <h2>What does LUC mean?</h2>
+                    <p>LUC is the abbreviation for Land Use Commission</p>
+                </div>
+            </div>
+
+</div>
         </div>
     </body>
 </html>
