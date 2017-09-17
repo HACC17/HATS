5e2e92d03d65c7e8b9ac71a49ee69db46041108f
gfarinas <gfarinas@hawaii.edu>
2017-09-16 21:01:33 -1000

    edited footer head

--- laravel/resources/views/includes/footer-head.blade.php
+++ laravel/resources/views/includes/footer-head.blade.php
@@ -1,22 +1,15 @@
 <style>
 	
 .footer-home {
-  position: relative;
+  position: absolute;
   font-size: 10pt;
   right: 0;
   bottom: 0;
   left: 0;
-  padding: 1rem;
+  padding: 0.5rem;
   background-color: rgba(248,248,248,0.8);
   overflow: hidden;
-  width: 80%;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 20px;
-  padding-left: 20px;
-  padding-right: 20px;
-  padding-bottom: 20px;
-  padding-top: 20px;
+  margin-bottom: auto;
 }
 
 .footer ul li {
@@ -46,39 +39,6 @@ ul.site {
 }
 </style>
 <div class="footer-home" id="copyright text-right">
-Copyright©2017&middot;All Rights Reserved&middot;Land Use Commission
-<br>235 South Beretania Street, Room 406 Honolulu, Hawai`i 96813;</br>	
+Copyright©2017 | All Rights Reserved | Land Use Commission | 235 South Beretania Street, Room 406 Honolulu, Hawai`i 96813;
 
-<ul class="site">
-        <li><b><u>About</b></u></li>
-        <li><a href="/about"> - About LUC</li></a>
-        <li> - Land Use Districts</li>
-        <li> - LUC Members and Staff</li>
-        <li><b><u>LUC Maps</b></u></li>
-        <li><b><u>Contact</b></u></li>
-</ul>
-<ul class="site">
-        <li><b><u><a href="/dockets">Dockets</a></u></b></li>
-        <li><a href="/pending"> - Pending Dockets</a></li>
-        <li><a href="/completed"> - Completed Dockets</a></li>
-        <li><a href="/search"> - Search for a Docket</a></li>
-                
-        <li><b><u>Self Service</li></b></u>
-        <li> - FAQ</li>
-        <li> - New User</li>
-        <li> - Returning User</li>
-</ul>
-<ul class="site">
-        <li><b><u>Procedures</li></u></b>
-        <li> - Affordable Housing Projects</li>
-        <li> - Decision-Making Criteria</li>
-        <li> - DBA Procedures</li>
-        <li> - Fifteen Acre Rule</li>
-        <li> - Quasi-judicial Proceedings</li>
-        <li> - Special Use Permit Procedures</li>
-</ul>
-<ul class="site">
-	<img src="imgs/misc_imgs/footer-seal.png" alt="footer seal" align="right");
-</ul>
-<div class="inner-footer"></div>
 </div>
