256c619bd0eb67d1997b1a5c36f1d284e45c0afa
KuakiniRob <kuakinir@gmail.com>
2017-09-16 21:56:12 -0700

    Added modals to first time page

--- laravel/resources/views/pages/first_time.blade.php
+++ laravel/resources/views/pages/first_time.blade.php
@@ -1,20 +1,20 @@
+
+
 @extends('layouts.default')
 @section('content')
-<html> 
-<style>
-html{
-height: 100%; 
-}
-
-body { 
-    /* Background Image found in laravel/public */
-    background-image: url('/imgs/self_service_imgs/land1.jpg');
-    height: 50%; 
-    background-position: center;
-    background-attachment: fixed;
-    background-size: cover; 
-}
-
+<html>
+    <style>
+        html{
+        height: 100%; 
+        }
+        body { 
+        /* Background Image found in laravel/public */
+        background-image: url('/imgs/self_service_imgs/land1.jpg');
+        height: 50%; 
+        background-position: center;
+        background-attachment: fixed;
+        background-size: cover; 
+        }
         .textcontainer {
         width: 70%;
         margin-right: auto;
@@ -23,89 +23,92 @@ body {
         margin-bottom: 1in;
         padding: 20px;
         background-color: rgba(248,248,248,0.8);
-border: 6px solid red;
+        border: 6px solid red;
         }
-.pagecontainer {
-border: 6px solid red;
-display: inline-block
-}
-/* Outer Boxes */
-.in-boxed {
-
-  text-align: center;
-  height: 300px;
-  display: inline-block;
-border: 6px solid red;
-} 
-
-/* Inner Boxes */
-.boxed {
-  vertical-align: middle;
-  display: inline-block;
-  width: 300px;
-  height: 150px;
-  padding: 20px;
-  margin: 10px;
-  background-color: #f8f8f8 ;
-
-} 
-/* Buttons */
-.button {
-    background-color: #f8f8f8;
-    color: black;
-    padding: 15px;
-    text-align: center;
-    text-decoration: none;
-    font-size: 16px;
-    margin: 4px 2px;
-    cursor: pointer;
-}
-
-.center {
- text-align: center;
-}
-
-.left {
- text-align: left;
-}
-
-.display {
-    visibility: hidden;
-}
-</style>
-<body> 
-<div class="pagecontainer">
-<div class="textcontainer">
-<h2> If you see a question below you want to know the answer to, click it for information.</h2>
-</div>
-
-<!-- Help Boxes -->
-<div class="in-boxed">
-
-<!-- QA01 -->
-<button class="button" onclick="ToggleVisibility('hide1'); return false;"><h3>What does "LUC" stand for?</h3></button>
-<div class="hide1 display button">LUC is the abbreviation for Land Use Commission.</div>
-
-<!-- QA02 -->
-<button class="button" onclick="ToggleVisibility('hide2'); return false;"><h3>Where can I learn more about the LUC?</h3></button>
-<div class="hide2 display button">Two</div>
-
-
-
-
-</div>
-</div>
-</body>
-<script>
-function ToggleVisibility(divClass)
-{
-    var els = document.getElementsByClassName(divClass);
-    for(var i = 0; i < els.length; i++)
-    {
-        els[i].style.visibility = els[i].style.visibility == "hidden" ? "visible" : "hidden";
-    }
-}
-</script>
-</html> 
+        .pagecontainer {
+        border: 6px solid red;
+        display: inline-block
+        }
+        .center {
+        text-align: center;
+        }
+        .left {
+        text-align: left;
+        }
+        .modalDialog {
+        position: fixed;
+        font-family: Arial, Helvetica, sans-serif;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        background: rgba(0,0,0,0.8);
+        z-index: 99999;
+        opacity:0;
+        -webkit-transition: opacity 400ms ease-in;
+        -moz-transition: opacity 400ms ease-in;
+        transition: opacity 400ms ease-in;
+        pointer-events: none;
+        }
+        .modalDialog:target {
+        opacity:1;
+        pointer-events: auto;
+        }
+        .modalDialog > div {
+        width: 400px;
+        position: relative;
+        margin: 10% auto;
+        padding: 5px 20px 13px 20px;
+        border-radius: 10px;
+        background: #fff;
+        background: -moz-linear-gradient(#fff, #999);
+        background: -webkit-linear-gradient(#fff, #999);
+        background: -o-linear-gradient(#fff, #999);
+        }
+        .close {
+        background: #606061;
+        color: #FFFFFF;
+        line-height: 25px;
+        position: absolute;
+        right: -12px;
+        text-align: center;
+        top: -10px;
+        width: 24px;
+        text-decoration: none;
+        font-weight: bold;
+        -webkit-border-radius: 12px;
+        -moz-border-radius: 12px;
+        border-radius: 12px;
+        -moz-box-shadow: 1px 1px 3px #000;
+        -webkit-box-shadow: 1px 1px 3px #000;
+        box-shadow: 1px 1px 3px #000;
+        }
+        .close:hover { background: #00d9ff; }
+    </style>
+    <body>
+        <div class="pagecontainer">
+            <div class="textcontainer">
+                <h2> If you see a question below you want to know the answer to, click it for information.</h2>
+            </div>
+            <a href="#openModal1">What does LUC mean?</a>
+            <div id="openModal1" class="modalDialog">
+                <div>
+                    <a href="#close" title="Close" class="close">X</a>
+                    <h2>What does LUC mean?</h2>
+                    <p>LUC is the abbreviation for Land Use Commission</p>
+                </div>
+            </div>
+            <a href="#openModal2">What services does LUC provide?</a>
+            <div id="openModal2" class="modalDialog">
+                <div>
+                    <a href="#close" title="Close" class="close">X</a>
+                    <h2>What services does LUC provide?</h2>
+                    <p>LUC provides assistance with land property information, processing this request, and compiling resources for the public.</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
 @stop
+
 
