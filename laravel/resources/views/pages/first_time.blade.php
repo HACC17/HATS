585640e75f0bf2b828d93672519780ba7e07df92
KuakiniRob <kuakinir@gmail.com>
2017-09-17 00:50:58 -0700

    Modified first time page

--- laravel/resources/views/pages/first_time.blade.php
+++ laravel/resources/views/pages/first_time.blade.php
@@ -15,6 +15,7 @@
         background-attachment: fixed;
         background-size: cover; 
         }
+
         .textcontainer {
         width: 70%;
         margin-right: auto;
@@ -23,8 +24,8 @@
         margin-bottom: 1in;
         padding: 20px;
         background-color: rgba(248,248,248,0.8);
-
         }
+
         .pagecontainer {
         }
         .center {
@@ -53,7 +54,8 @@
         pointer-events: auto;
         }
         .modalDialog > div {
-        width: 400px;
+        width: 500px;
+        height: 500;
         position: relative;
         margin: 10% auto;
         padding: 5px 20px 13px 20px;
@@ -102,8 +104,15 @@ border: 1px solid red;
             <div class="textcontainer">
                 <h2> If you see a question below you want to know the answer to, click it for information.</h2>
             </div>
+
+
 <div class="modalBox">
-<!-- Modal:01 -->
+<!-- Start of Table -->
+<table>
+<!-- Table row one -->
+<tr>
+<!-- Modal:01: What does LUC mean?-->
+<td>
             <a class="modalCon" href="#openModal1"><p>What does LUC mean?</p></a>
             <div id="openModal1" class="modalDialog">
                 <div>
@@ -112,7 +121,9 @@ border: 1px solid red;
                     <p>LUC is the abbreviation for Land Use Commission</p>
                 </div>
             </div>
-<!-- Modal:02 -->
+</td>
+<td>
+<!-- Modal:02: What services does LUC provide? -->
             <a class="modalCon" href="#openModal2">What services does LUC provide?</a>
             <div id="openModal2" class="modalDialog">
                 <div>
@@ -121,83 +132,113 @@ border: 1px solid red;
                     <p>LUC provides assistance with land property information, processing this request, and compiling resources for the public.</p>
                 </div>
             </div>
-<!-- Modal:03 -->
+</td>
+<td>
+<!-- Modal:03: Where can I learn more about LUC? -->
             <a class="modalCon" href="#openModal3">Where can I learn more about LUC?</a>
             <div id="openModal3" class="modalDialog">
                 <div>
                     <a href="#close" title="Close" class="close">X</a>
-                    <h2>What does LUC mean?</h2>
-                    <p>LUC is the abbreviation for Land Use Commission</p>
+                    <h2>Where can I learn more about LUC?</h2>
+                    <a href="/about"><p>You can learn more about the LUC in the about section.Click here to go directly there.</p></a>
                 </div>
             </div>
+</td>
+</tr>
+<!-- Second Row -->
+<tr>
+<th>
 <!-- Modal:04 -->
             <a class="modalCon" href="#openModal4">What are dockets?</a>
             <div id="openModal4" class="modalDialog">
                 <div>
                     <a href="#close" title="Close" class="close">X</a>
-                    <h2>What does LUC mean?</h2>
-                    <p>LUC is the abbreviation for Land Use Commission</p>
+                    <h2>What are dockets?</h2>
+                    <p>Dockets a summary of information related to specific property or case. </p>
                 </div>
             </div>
+</th>
+<th>
 <!-- Modal:05 -->
             <a class="modalCon" href="#openModal5">What do I need to process a request?</a>
             <div id="openModal5" class="modalDialog">
                 <div>
                     <a href="#close" title="Close" class="close">X</a>
-                    <h2>What does LUC mean?</h2>
-                    <p>LUC is the abbreviation for Land Use Commission</p>
+                    <h2>What do I need to process a request?</h2>
+                    <p>You will need to have the following:
+            <ul>
+                <li>Full Name</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+                <li>Organization</li>
+                <li>Type of information you are requesting or submitting</li>
+                <li>Tax Map Key</li>
+                <li>Docket ID</li>
+            </ul>
+</p>
                 </div>
             </div>
-
+</th>
+<th>
 <!-- Modal:06 -->
             <a class="modalCon" href="#openModal6">Where can I get additional assistance?</a>
             <div id="openModal6" class="modalDialog">
                 <div>
                     <a href="#close" title="Close" class="close">X</a>
-                    <h2>What does LUC mean?</h2>
-                    <p>LUC is the abbreviation for Land Use Commission</p>
+                    <h2>Where can I get additional assistance?</h2>
+                    <p>LUC offers assistance by email, phone or in person. You can visit the contact tab for this information or<a href="/contact">click here to go directly there.</a></p>
                 </div>
             </div>
-
+</th>
+</tr>
+<!-- Third Row -->
+<tr>
+<th>
 <!-- Modal:07 -->
             <a class="modalCon" href="#openModal7">What is a Tax map key?</a>
             <div id="openModal7" class="modalDialog">
                 <div>
                     <a href="#close" title="Close" class="close">X</a>
-                    <h2>What does LUC mean?</h2>
-                    <p>LUC is the abbreviation for Land Use Commission</p>
+                    <h2>What is a Tax map key?</h2>
+                    <p>
+            <ul>
+                <li>A Tax Map Key is a unique property identifier number.</li>
+                <li>It is used by staff to assist you.</li>
+                <li>Every legal piece of property is linked to a Tax Map Key.</li>
+                <li>Allows you to search files on the Land Use Commission website. </li>
+                <li>Allows you to search ArcGIS online mapping applications. </li>
+                <li>Used to find information about a property. </li>
+                <li>Used to request a service. </li>
+            </ul>
+</p>
                 </div>
             </div>
-
+</th>
+<th>
 <!-- Modal:08 -->
             <a class="modalCon" href="#openModal8">Where is LUC located?</a>
             <div id="openModal8" class="modalDialog">
                 <div>
                     <a href="#close" title="Close" class="close">X</a>
-                    <h2>What does LUC mean?</h2>
-                    <p>LUC is the abbreviation for Land Use Commission</p>
+                    <h2>Where is LUC located?</h2>
+                    <p>LUC is located the following location:<br>
+State Office Towe Leiopapa A Kamehameha Building 235 South Beretania Street, Room 406<br> 
+Honolulu,Hawai`i 96804-2359. </p>
                 </div>
             </div>
-
+</th>
+<th>
 <!-- Modal:09 -->
             <a class="modalCon" href="#openModal9">How do I obtain a Tax map key?</a>
             <div id="openModal9" class="modalDialog">
                 <div>
                     <a href="#close" title="Close" class="close">X</a>
-                    <h2>What does LUC mean?</h2>
-                    <p>LUC is the abbreviation for Land Use Commission</p>
+                    <h2>How do I obtain a Tax map key?</h2>
+                    <p>LUC utilizes ARCGis to select and retrieve a locations Tax Map Key by coordinates.</p>
                 </div>
             </div>
-
-<!-- Modal:10 -->
-            <a class="modalCon" href="#openModal10">What are LUC maps?</a>
-            <div id="openModal10" class="modalDialog">
-                <div>
-                    <a href="#close" title="Close" class="close">X</a>
-                    <h2>What does LUC mean?</h2>
-                    <p>LUC is the abbreviation for Land Use Commission</p>
-                </div>
-            </div>
+</th>
+</tr>
 
 </div>
         </div>
