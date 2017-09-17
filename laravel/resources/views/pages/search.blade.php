7ed18fbc8002d83f11103b3fba70fa5c2eef60cf
sartecat <30540778+sartecat@users.noreply.github.com>
2017-09-17 13:46:50 -1000

    Update search.blade.php

--- laravel/resources/views/pages/search.blade.php
+++ laravel/resources/views/pages/search.blade.php
@@ -1,3 +1,36 @@
+@extends('layouts.default')
+@section('content')
+<style>
+body { 
+    background-image: url('imgs/dockets_imgs/land1.jpg');
+    height: 50%; 
+    background-position: center;
+    background-attachment: fixed;
+    background-size: cover; 
+}
+table {
+    border-collapse: collapse;
+    width: 70%;
+}
+
+th, td {
+    padding: 8px;
+    text-align: left;
+    border: 1px solid #ddd;
+}
+
+
+th {
+    background-color: #bce8ff;
+}
+
+tr:hover{background-color:#f5f5f5}
+</style>
+
+<body>
+<div class="textcontainer">
+<h2>Search Results:</h2>
+<p>Click on the docket number to view information about the docket.</p>
 <?php
 
 	$docketType = NULL;
@@ -125,3 +158,10 @@
 	}
 ?>
 </table>
+<br>
+<form>
+<button>Search again</button>
+</form>
+
+</div>
+@stop
