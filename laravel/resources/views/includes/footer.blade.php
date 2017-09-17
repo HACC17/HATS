d8bf068523584f1bba0cd0a735dc258d7185327c
UbuntuVM <sartecat@hawaii.edu>
2017-09-16 19:24:01 -1000

    home footer

--- laravel/resources/views/includes/footer.blade.php
+++ laravel/resources/views/includes/footer.blade.php
@@ -1,6 +1,24 @@
 <style>
+.footer-home {
+  position: relative;
+  font-size: 10pt;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  padding: 1rem;
+  background-color: rgba(248,248,248,0.8);
+  overflow: hidden;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-bottom: 20px;
+  padding-top: 20px;
+}
 
-.footer {
+.footer-other {
   position: sticky;
   font-size: 10pt;
   right: 0;
@@ -17,19 +35,68 @@
   padding-right: 20px;
   padding-bottom: 20px;
   padding-top: 20px;
-  background: url(imgs/misc_imgs/footer-seal.png) right no-repeat;
+  color: white;
 }
 
 .footer ul li {
-	list-style-type: none;
+  list-style-type: none;
+  margin: 2px;
+}
+ul.site {
+    list-style-type: none;
+    margin: 10px;
+    padding: 0;
+    margin: 0.25em auto;
+    vertical-align: top;
+    display: inline-block;
+    width: 25%;
+    float: left;
 }
 
+.inner-footer {
+    background: url(imgs/misc_imgs/footer-seal.png) top right no-repeat;
+    margin: 0 2%;
+    margin-top: 0px;
+    margin-right: 2%;
+    margin-bottom: 0px;
+    margin-left: 2%;
+}
 </style>
-<div class="footer" id="copyright text-right">
-Copyright©2017&middot;All Rights Reserved&middot;Land Use Commission<
+<div class="footer-other" id="copyright text-right">
+Copyright©2017&middot;All Rights Reserved&middot;Land Use Commission
 <br>235 South Beretania Street, Room 406 Honolulu, Hawai`i 96813;</br>	
-<ul>
-	<li>This is it</li>
-	<li>This is it</li>
+
+<ul class="site">
+        <li><b><u>About</b></u></li>
+        <li><a href="/about"> - About LUC</li></a>
+        <li> - Land Use Districts</li>
+        <li> - LUC Members and Staff</li>
+        
+        <li><b><u>LUC Maps</b></u></li>
+        <li><b><u>Contact</b></u></li>
 </ul>
+<ul class="site">
+        <li><a href="/dockets"><b><u>Dockets</li></b></u></a>
+        <li><a href="/pending"> - Pending Dockets</li></a>
+        <li><a href="/completed"> - Completed Dockets</li></a>
+        <li><a href="/search"> - Search for a Docket</li></a>
+                
+        <li><b><u>Self Service</li></b></u>
+        <li> - FAQ</li>
+        <li> - New User</li>
+        <li> - Returning User</li>
+</ul>
+<ul class="site">
+        <li><b><u>Procedures</li></u></b>
+        <li> - Affordable Housing Projects</li>
+        <li> - Decision-Making Criteria</li>
+        <li> - DBA Procedures</li>
+        <li> - Fifteen Acre Rule</li>
+        <li> - Quasi-judicial Proceedings</li>
+        <li> - Special Use Permit Procedures</li>
+</ul>
+<ul class="site">
+	<img src="imgs/misc_imgs/footer-seal.png" alt="footer seal" align="right");
+</ul>
+<div class="inner-footer"></div>
 </div>
