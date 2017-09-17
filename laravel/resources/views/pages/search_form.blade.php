18897ed0673973cf599e01bd2c8f5551c968081d
sartecat <30540778+sartecat@users.noreply.github.com>
2017-09-17 13:47:35 -1000

    Update search_form.blade.php

--- laravel/resources/views/pages/search_form.blade.php
+++ laravel/resources/views/pages/search_form.blade.php
@@ -1,7 +1,18 @@
-<!DOCTYPE html>
-<html>
-<body>
-
+@extends('layouts.default')
+@section('content')
+<style>
+body { 
+    background-image: url('imgs/dockets_imgs/land1.jpg');
+    height: 50%; 
+    background-position: center;
+    background-attachment: fixed;
+    background-size: cover; 
+}
+</style>
+<div class='textcontainer'>
+<h2>Looking for a docket?</h2>
+<p>Enter the docket number, project name, or docket name and press the Search button.</p>
+<hr>
 <form action="" method="post">
 	{{ csrf_field() }}
 	<p>
@@ -12,21 +23,19 @@
 			<option value="SP">SP</option>
 			<option value="DR">DR</option>
 		</select>
-		<input type="text" name="docketYear" maxlength="2" size="2">
+		<input type="text" name="docketYear" maxlength="2" size="2" placeholder="##">
 		-
-		<input type="text" name="docketNumber" maxlength="4" size="4">
+		<input type="text" name="docketNumber" maxlength="4" size="4" placeholder="####">
 	</p>
 	<p>
 		Docket Name: 
-		<input type="text" name="docketName">
+		<input type="text" name="docketName" placeholder="Docket Name">
 	</p>
 	<p>
 		Project Name: 
-		<input type="text" name="project">
+		<input type="text" name="project" placeholder ="Project Name">
 	</p>
-	<input type="submit">
+	<button>Search</button>
 </form>
-
-</body>
-</html>
-
+</div>
+@stop
