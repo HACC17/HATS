bf8e5577833f49a5c2bad8734b14beaefda2c2c5
gfarinas <gfarinas@hawaii.edu>
2017-09-17 15:35:48 -1000

    removed Return menu

--- laravel/resources/views/includes/header.blade.php
+++ laravel/resources/views/includes/header.blade.php
@@ -153,7 +153,6 @@ ul.topnav li.right {float: right;}
         <div class="dropdown-content">
         <a href="faq">FAQ</a>
         <a href="first_time">First Time</a>
-        <a href="returning">Returning</a>
         </div>
       </div>
     </div>
